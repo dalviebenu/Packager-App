890eb3a60b145a355da1eead54ce608f2e68ffbd
benu <dalvie.benu@gmail.com>
2023-07-01 16:16:28 +0200

    Added changes to allow 5-10 screenshots and update manifest.json data.

--- art.py
+++ art.py
@@ -23,9 +23,9 @@ class StoreArt:
         path = Path(path)
 
         thumbnail = Image(path / 'S0.jpg', MEDIUM)
-        screenshots = (Image(path / 'S{}.jpg'.format(i), MEDIUM) for i in range(1, 6)) if world else None
-        panorama = Image(path / 'S6.jpg', PANORAMA) if world else None
-        icon = Image(path / 'S7.jpg', L_SQ) if world and (path / 'S7.jpg').is_file() else None
+        screenshots = (Image(path / 'S{}.jpg'.format(i), MEDIUM) for i in range(1, 11)) if world else None
+        panorama = Image(path / 'pano.jpg', PANORAMA) if world else None
+        icon = Image(path / 'S11.jpg', L_SQ) if world and (path / 'S11.jpg').is_file() else None
 
         return cls(thumbnail, icon, panorama, screenshots, fargs)
 
@@ -58,8 +58,8 @@ class MarketingArt:
     @classmethod
     def load(cls, path: Path, world, fargs):
         key_art = Image(path / 'M0.jpg', HD)
-        screenshots = (Image(path / 'M{}.jpg'.format(i), HD) for i in range(1, 6)) if world else None
-        partner_art = Image(path / 'M6.jpg', HD)
+        screenshots = (Image(path / 'M{}.jpg'.format(i), HD) for i in range(1, 11)) if world else None
+        partner_art = Image(path / 'banner.jpg', HD)
 
         return cls(key_art, screenshots, partner_art, fargs)
 
